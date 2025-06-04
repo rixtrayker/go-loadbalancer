92b13e6e63bf996e6c4b5af75c3404da78086787
Amr M. Elsayed <cs.elsayed@gmail.coms>
2025-06-04 04:13:49 +0300

    Update flake.nix to upgrade Go version from 1.21 to 1.24 in development shell

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            go_1_21
+            go_1_24
             gopls
             go-outline
             gocode
